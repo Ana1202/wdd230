72577506c0f4240234611ac7c136d22eb294f237
Ana1202 <nayeli.snchz03@gmail.com>
2022-05-03 16:10:07 -0500

    update pic name

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,7 @@ document.querySelector('#name').textContent= myName ;
 let yearnow= '2022'
 document.querySelector('#year').textContent = yearnow ;
 // pic 
-let myPicture = 'images/anasphoto.jpg';
+let myPicture = 'images/ana.jpg';
 document.querySelector('img').setAttribute('src', myPicture);
 
 //modifycation date and time 
