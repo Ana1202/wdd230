2a87cfabdf5586a2f128480079e4d3e315535428
Ana1202 <nayeli.snchz03@gmail.com>
2023-05-04 23:08:55 -0500

    chamber of Commerce week 3 lesson 3

---
+++ lesson3/js/index.js
@@ -0,0 +1,16 @@
+// Setting up my variables 
+//Logo
+let siteLogo = 'images/sitelogo.png';
+document.querySelector('img').setAttribute('src', siteLogo);
+
+//domain
+let siteDomain= 'www.evanstownchamber.com';
+document.querySelector('#domain').textContent= siteDomain;
+
+//declering year 
+let yearnow= '2023'
+document.querySelector('#year').textContent = yearnow ;
+
+//modifycation date and time 
+let oLastModif = new Date(document.lastModified);
+document.querySelector('#mydatetime').textContent= oLastModif ;
