129567b356ac5512266b78f828c5ba20f81ef58f
Ana1202 <nayeli.snchz03@gmail.com>
2023-05-09 22:14:32 -0500

    js

--- lesson3/scripts/wp.js
+++ lesson3/scripts/wp.js
@@ -9,7 +9,7 @@ document.querySelector('#domain').textContent= siteDomain;
 
 //declering year 
 let yearnow= '2023'
-document.querySelector('#year').textContent = yearnow ;
+document.getElementById('year').textContent = yearnow ;
 
 //modifycation date and time 
 let oLastModif = new Date(document.lastModified);
