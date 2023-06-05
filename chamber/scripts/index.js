8b21d9333ad0bd0171ed939b6629a35492f1d618
Ana1202 <nayeli.snchz03@gmail.com>
2023-06-05 10:11:05 -0500

    correted date to DMY

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -9,9 +9,6 @@ const datefieldUK = document.querySelector("aside"); // for european/family hist
 
 // derive the current date using a date object
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
